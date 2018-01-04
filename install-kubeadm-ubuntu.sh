256cc92255ad602b7b812ce541ba1c559b65c70f
Korey Earl <korey.earl@gmail.com>
2018-01-04 17:21:45 +0000

    add sudo to all commands in install script

--- install-kubeadm-ubuntu.sh
+++ install-kubeadm-ubuntu.sh
@@ -1,14 +1,14 @@
 #! /bin/bash -xe
 
 # Install Docker
-apt-get update
-apt-get install -y docker.io
+sudo apt-get update
+sudo apt-get install -y docker.io
 
 #Install kubeadm, kubelet and kubectl
-apt-get update && apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
+sudo apt-get install -y kubelet kubeadm kubectl
