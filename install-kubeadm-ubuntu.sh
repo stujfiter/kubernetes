4aed72ee209ecd42c0a60d3e481d673164b1394e
Korey Earl <korey.earl@gmail.com>
2018-01-04 07:54:18 -0600

    Initial Commit

---
+++ install-kubeadm-ubuntu.sh
@@ -0,0 +1,17 @@
+#! /bin/bash -xe
+
+# Install Docker
+apt-get update
+apt-get install -y docker.io
+
+#Install kubeadm, kubelet and kubectl
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+
+#Initialize Master
+kubeadm init
