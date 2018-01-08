3829589fc0a018f9a7492e0743b5d58ebd4fd180
Korey Earl <korey.earl@gmail.com>
2018-01-08 15:29:10 -0600

    Add script to create Azure Jump Box from bash

---
+++ azure/jumpbox/delete.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# -e: immediately exit if any command has a non-zero exit status
+# -o: prevents errors in a pipeline from being masked
+# IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
+
+usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation>" 1>&2; exit 1; }
+
+declare subscriptionId=""
+declare resourceGroupName=""
+declare deploymentName=""
+declare resourceGroupLocation=""
+
+# Initialize parameters specified from command line
+while getopts ":i:g:n:l:" arg; do
+	case "${arg}" in
+		i)
+			subscriptionId=${OPTARG}
+			;;
+		g)
+			resourceGroupName=${OPTARG}
+			;;
+		esac
+done
+shift $((OPTIND-1))
+
+#Prompt for parameters if some required parameters are missing
+if [[ -z "$subscriptionId" ]]; then
+	echo "Subscription Id:"
+	read subscriptionId
+	[[ "${subscriptionId:?}" ]]
+fi
+
+if [[ -z "$resourceGroupName" ]]; then
+	echo "ResourceGroupName:"
+	read resourceGroupName
+	[[ "${resourceGroupName:?}" ]]
+fi
+
+if [ -z "$subscriptionId" ] || [ -z "$resourceGroupName" ]; then
+	echo "Either one of subscriptionId, resourceGroupName, deploymentName is empty"
+	usage
+fi
+
+set +e
+
+#login to azure using your credentials
+az account show 1> /dev/null
+
+if [ $? != 0 ];
+then
+	az login
+fi
+
+#set the default subscription id
+az account set --subscription $subscriptionId
+
+#delete the resource group
+az group delete --name $resourceGroupName --yes
