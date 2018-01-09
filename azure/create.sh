193097f850a8c61f2c3fd192b0ebefe882db9fb2
Korey Earl <korey.earl@gmail.com>
2018-01-09 12:21:35 -0600

    Added VPC Template, refactored file structure for better re-use of create.sh and delete.sh

--- azure/create.sh
+++ azure/create.sh
@@ -6,15 +6,15 @@ IFS=$'\n\t'
 # -o: prevents errors in a pipeline from being masked
 # IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
 
-usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation>" 1>&2; exit 1; }
+usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -l <resourceGroupLocation> -t <templateFilePath>" 1>&2; exit 1; }
 
 declare subscriptionId=""
 declare resourceGroupName=""
-declare deploymentName=""
 declare resourceGroupLocation=""
+declare templateFilePath=""
 
 # Initialize parameters specified from command line
-while getopts ":i:g:n:l:" arg; do
+while getopts ":i:g:n:l:t:" arg; do
 	case "${arg}" in
 		i)
 			subscriptionId=${OPTARG}
@@ -22,53 +22,28 @@ while getopts ":i:g:n:l:" arg; do
 		g)
 			resourceGroupName=${OPTARG}
 			;;
-		n)
-			deploymentName=${OPTARG}
-			;;
 		l)
 			resourceGroupLocation=${OPTARG}
+			;;
+		t)
+			templateFilePath=${OPTARG}
 			;;
 		esac
 done
 shift $((OPTIND-1))
 
-#Prompt for parameters if some required parameters are missing
-if [[ -z "$subscriptionId" ]]; then
-	echo "Subscription Id:"
-	read subscriptionId
-	[[ "${subscriptionId:?}" ]]
+#Verify all parameters have been entered
+if [ -z "$subscriptionId" ] || [ -z "$resourceGroupName" ] || [ -z "$templateFilePath" ]; then
+	echo "Either one of subscriptionId, resourceGroupName, or templateFilePath is empty"
+	usage
 fi
-
-if [[ -z "$resourceGroupName" ]]; then
-	echo "ResourceGroupName:"
-	read resourceGroupName
-	[[ "${resourceGroupName:?}" ]]
-fi
-
-if [[ -z "$deploymentName" ]]; then
-	echo "DeploymentName:"
-	read deploymentName
-fi
-
-if [[ -z "$resourceGroupLocation" ]]; then
-	echo "Enter a location below to create a new resource group else skip this"
-	echo "ResourceGroupLocation:"
-	read resourceGroupLocation
-fi
-
-#templateFile Path - template file to be used
-templateFilePath="template.json"
 
 if [ ! -f "$templateFilePath" ]; then
 	echo "$templateFilePath not found"
 	exit 1
 fi
 
-if [ -z "$subscriptionId" ] || [ -z "$resourceGroupName" ] || [ -z "$deploymentName" ]; then
-	echo "Either one of subscriptionId, resourceGroupName, deploymentName is empty"
-	usage
-fi
-
+#Do not exit if the following commands return non-zero
 set +e
 
 #login to azure using your credentials
@@ -101,7 +76,7 @@ fi
 echo "Starting deployment..."
 (
 	set -x
-	az group deployment create --name $deploymentName --resource-group $resourceGroupName --template-file $templateFilePath
+	az group deployment create --resource-group $resourceGroupName --template-file $templateFilePath
 )
 
 if [ $?  == 0 ];
--- azure/delete.sh
+++ azure/delete.sh
@@ -6,12 +6,10 @@ IFS=$'\n\t'
 # -o: prevents errors in a pipeline from being masked
 # IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
 
-usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -l <resourceGroupLocation>" 1>&2; exit 1; }
+usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName>" 1>&2; exit 1; }
 
 declare subscriptionId=""
 declare resourceGroupName=""
-declare deploymentName=""
-declare resourceGroupLocation=""
 
 # Initialize parameters specified from command line
 while getopts ":i:g:n:l:" arg; do
