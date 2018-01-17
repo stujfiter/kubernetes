d4770a40aa90c19b94881e71e572d04b08a93aff
Korey Earl <korey.earl@gmail.com>
2018-01-17 11:43:23 -0600

    Install Azure CLI on kubecluster vms

--- azure/stacks/kubecluster/include.sh
+++ azure/stacks/kubecluster/include.sh
@@ -3,7 +3,8 @@ cloudInitData() {
 }
 
 outputs() {
+    echo "Kubernetes Master IP"
+    az network nic show -g $resourceGroupName -n kube-master-nic | jq -r '.ipConfigurations[0].privateIpAddress'
     echo "Kubernetes Cluster Instance IPs"
     az vmss list-instances -g $resourceGroupName -n KubeScaleSet | jq '.[].id' | xargs az vmss nic list-vm-nics -g $resourceGroupName --vmss-name KubeScaleSet --ids | jq -r '.[] | .[] | .ipConfigurations[] | .privateIpAddress'
-    echo ""
 }
