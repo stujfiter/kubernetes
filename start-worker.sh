0cd1cb2ca9da14f05c05f03cf94eaf3b874075ff
Korey Earl <korey.earl@gmail.com>
2018-01-20 23:48:46 -0600

    Join worker nodes to master

--- start-worker.sh
+++ start-worker.sh
@@ -1,3 +1,5 @@
 #! /bin/bash -x
 
 netcat -l 5100 & netcat -l 5101 > cluster-token.txt
+. cluster-token.txt
+sudo kubeadm join --token $clusterToken --discovery-token-unsafe-skip-ca-verification $masterIp:6443
