afbc743d3505eb32530991fa69226014abccaf47
Korey Earl <korey.earl@gmail.com>
2018-01-20 23:16:14 -0600

    Send the Cluster Token and the IP Address to all nodes

--- start-master.sh
+++ start-master.sh
@@ -8,6 +8,9 @@ chown kearl:kearl /home/kearl/.kube/config
 runuser -l kearl -c 'kubectl apply -f /kubernetes/kube-flannel.yml'
 
 #Send message to all worker nodes listening on port 5100
-export clusterToken="$(sudo kubeadm token list | grep 'default-node-token' | awk '{ print $1 }')"
+clusterToken="$(sudo kubeadm token list | grep 'default-node-token' | awk '{ print $1 }')"
+masterIp="$(hostname --ip-address)"
+export clusterInfo=$"clusterToken=$clusterToken\nmasterIp=$masterIp"
+
 apt-get install -y nmap
-nmap -Pn -p5100 --open 10.0.0.0/24 | grep "10.0.0" | awk '{ print $5 }' | xargs -I '%' sh -c 'echo "$clusterToken" | nc -w1 % 5101'
+nmap -Pn -p5100 --open 10.0.0.0/24 | grep "10.0.0" | awk '{ print $5 }' | xargs -I '%' sh -c 'echo "$clusterInfo" | nc -w1 % 5101'
